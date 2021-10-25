83ccb4b23ea9b22154f4cf048cb753b3be891835
franzihe <franziska.hellmuth@geo.uio.no>
2021-10-25 19:49:36 +0200

    downloaded variables: tclw, tciw

--- download_Amon_single_level.py
+++ download_Amon_single_level.py
@@ -21,63 +21,94 @@
 import cdsapi
 import glob
 
-path = '/home/franzihe/nird_franzihe/data/ERA5/'
-path = '/home/franzihe/nird_ERA5/'
+# path = '/home/franzihe/nird_franzihe/data/ERA5/'
+# path = '/home/franzihe/nird_ERA5/'
+path = "/scratch/franzihe/input/ERA5/monthly_means/0.25deg/"
 
-form = 'netcdf' # 'grib'
+form = "netcdf"  # 'grib'
 
-available_month = {'01_12':['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12']}
+available_month = {
+    "01_12": ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
+}
 
 available_years = {
-                   "85-89": ['1985','1986','1987','1988','1989',],
-                   "90-99": ['1990','1991','1992','1993','1994','1995','1996','1997','1998','1999',],
-                   "00-09": ['2000','2001','2002','2003','2004','2005','2006','2007','2008','2009',],
-                   "10-14": ['2010','2011','2012','2013','2014',]
-                    }
+    "85-89": ["1985", "1986", "1987", "1988", "1989",],
+    "90-99": [
+        "1990",
+        "1991",
+        "1992",
+        "1993",
+        "1994",
+        "1995",
+        "1996",
+        "1997",
+        "1998",
+        "1999",
+    ],
+    "00-09": [
+        "2000",
+        "2001",
+        "2002",
+        "2003",
+        "2004",
+        "2005",
+        "2006",
+        "2007",
+        "2008",
+        "2009",
+    ],
+    "10-14": ["2010", "2011", "2012", "2013", "2014",],
+}
 
-variable = {'2t':   '2m_temperature', 
-            'msr'  :'mean_snowfall_rate',
-            'sf'   :'snowfall', }
+variable = {
+    # "2t": "2m_temperature",
+    # "msr": "mean_snowfall_rate",
+    # "sf": "snowfall",
+    # "tclw": "total_column_cloud_liquid_water",
+    "tciw": "total_column_cloud_ice_water"
+}
 
 counter = 0
-for keys, var in variable.items(): 
-    for years in (available_years):
+for keys, var in variable.items():
+    for years in available_years:
         for months in available_month:
-         #   print(var, available_years[years], available_month[months])
-            
-            if form == 'netcdf':
-                _ff = 'nc'
-            elif form == 'grib':
-                _ff = 'grib'
+            #   print(var, available_years[years], available_month[months])
 
+            if form == "netcdf":
+                _ff = "nc"
+            elif form == "grib":
+                _ff = "grib"
 
-            filename = '{keys}_Amon_ERA5_{starty}{startm}_{endy}{endm}.{format}'.format(keys = keys,
-                                                                                   starty = available_years[years][0],
-                                                                                   startm = available_month[months][0],
-                                                                                   endy = available_years[years][-1], 
-                                                                                   endm = available_month[months][-1], 
-                                                                                      format = _ff)
-#            print(filename)
+            filename = "{keys}_Amon_ERA5_{starty}{startm}_{endy}{endm}.{format}".format(
+                keys=keys,
+                starty=available_years[years][0],
+                startm=available_month[months][0],
+                endy=available_years[years][-1],
+                endm=available_month[months][-1],
+                format=_ff,
+            )
+            #            print(filename)
 
-            files = glob.glob(path+filename)
+            files = glob.glob(path + filename)
             if path + filename in files:
-                print(path + filename + ' is downloaded')
+                print(path + filename + " is downloaded")
                 counter += 1
                 print("Have downloaded in total : " + str(counter) + " files")
             else:
                 c = cdsapi.Client()
 
-            c.retrieve(
-                'reanalysis-era5-single-levels-monthly-means',
-                {
-                    'product_type': 'monthly_averaged_reanalysis',
-                    'variable': var,
-                    'year': available_years[years],
-                    'month': available_month[months],
-                    'time': '00:00',
-#                    'grid': '0.25/0.25',        # Regrid from the default grid to a regular lat/lon with specified resolution. The first number is east-west resolution (longitude) and the second is north-south (latitude).
-                    'format': form,
-                    'stream': 'moda'
-                },
-                path + filename)
-            print('File saved: {}'.format(path + filename))
+                c.retrieve(
+                    "reanalysis-era5-single-levels-monthly-means",
+                    {
+                        "product_type": "monthly_averaged_reanalysis",
+                        "variable": var,
+                        "year": available_years[years],
+                        "month": available_month[months],
+                        "time": "00:00",
+                        #                    'grid': '0.25/0.25',        # Regrid from the default grid to a regular lat/lon with specified resolution. The first number is east-west resolution (longitude) and the second is north-south (latitude).
+                        "format": form,
+                        "stream": "moda",
+                    },
+                    path + filename,
+                )
+            print("File saved: {}".format(path + filename))
